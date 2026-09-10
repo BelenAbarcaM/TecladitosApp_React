9c81d8d31356fc2fb06cca8761164e944d173f75
BelenAbarcaM <belenabarcamonge@gmail.com>
2026-09-10 08:49:40 -0600

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,13 +8,13 @@ function verificarToken(req, res, next) {
     return res.status(401).json({ error: 'Token requerido' });
   }
 
-  const token = authHeader.split(' ')[1];  // Espera formato "Bearer token"
+  const token = authHeader.split(' ')[1];
 
   try {
-    // Usamos la misma variable (SECRETO) con la que authController firma el token
+
     const decoded = jwt.verify(token, process.env.SECRETO);
-    req.usuarioId = decoded.id;              // Guardamos el id del token en la request para usarlo después
-    next();                                  // Token válido, continuar a la siguiente función
+    req.usuarioId = decoded.id;              
+    next();                               
   } catch (err) {
     return res.status(403).json({ error: 'Token inválido o expirado' });
   }
